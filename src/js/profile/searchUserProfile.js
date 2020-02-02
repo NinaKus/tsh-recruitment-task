7df8cb62dcdde4a791bfcbdc196b9c5af14ddad6
Nina Kuś <ninakus.poczta@gmail.com>
2020-02-02 22:06:16 +0100

    Validation and adding red border to not valid input

--- src/js/profile/searchUserProfile.js
+++ src/js/profile/searchUserProfile.js
@@ -1,15 +1,22 @@
 import request from '../common/request';
 import updateProfile from './updateUserProfile';
+import { validateProfile } from "../common/validation";
 
 const searchUserProfile = () => {
   const loadButton = document.querySelector('.js-load-username');
   loadButton.addEventListener('click', () => {
-    const userName = document.querySelector('.js-username-input').value;
+    const userNameInput = document.querySelector('.js-username-input');
+    const userName = userNameInput.value;
 
-    request(`https://api.github.com/users/${userName}`)
-      .then(userDetails => {
-        updateProfile(userDetails);
-      });
+    if (validateProfile(userName)) {
+      userNameInput.classList.remove('has-error');
+      request(`https://api.github.com/users/${userName}`)
+        .then(userDetails => {
+          updateProfile(userDetails);
+        });
+    } else {
+      userNameInput.classList.add('has-error');
+    }
   });
 };
 
