5e9eb8233918bc7e686967c2381d9f7b99339d71
Nina Kuś <ninakus.poczta@gmail.com>
2020-02-02 23:56:35 +0100

    Hiding fake Profile and History fields until user data is loaded

--- src/js/profile/searchUserProfile.js
+++ src/js/profile/searchUserProfile.js
@@ -9,17 +9,22 @@ const searchUserProfile = () => {
   loadButton.addEventListener('click', () => {
     const userNameInput = document.querySelector('.js-username-input');
     const userName = userNameInput.value;
+    const spinner = document.querySelector('.js-spinner');
 
     if (validateProfile(userName)) {
       userNameInput.classList.remove('has-error');
+      spinner.classList.remove('is-hidden');
       request(`https://api.github.com/users/${userName}`)
         .then(userDetails => {
           updateProfile(userDetails);
+          document.querySelector('.js-profile').classList.remove('is-hidden');
         });
 
       getUserHistory(userName, ['PullRequestEvent', 'PullRequestReviewCommentEvent'])
         .then(userHistory => {
           updateUserHistory(userHistory);
+          spinner.classList.add('is-hidden');
+          document.querySelector('.js-user-timeline').classList.remove('is-hidden');
         });
     } else {
       userNameInput.classList.add('has-error');
